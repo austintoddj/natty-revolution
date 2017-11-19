1bd133babb0499de7c27951b304c7c77a2ea6a45
Todd Austin <austin.todd.j@gmail.com>
2017-11-18 22:09:29 -0600

    Content updates and refactors

--- resources/views/public/forms/contact.blade.php
+++ resources/views/public/forms/contact.blade.php
@@ -1,55 +1,30 @@
-<form class="form-horizontal" method="POST" action="{{ url('/') }}">
+<form method="POST" action="{{ url('/') }}">
     {{ csrf_field() }}
 
-    <div class="form-group row">
-        <label for="name" class="col-lg-4 col-form-label text-lg-right">Name</label>
-
-        <div class="col-lg-6">
-            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required>
-
-            @if ($errors->has('name'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('name') }}</strong>
-                </div>
-            @endif
-        </div>
+    <div class="text-ribbon-inputs-split">
+        <input id="name" type="text" class="h4 text-ribbon-input text-ribbon-primary{{ $errors->has('name') ? ' is-invalid' : '' }}" @if ($errors->has('name')) placeholder="{{ $errors->first('name') }}" @else placeholder="Name" @endif name="name" value="{{ old('name') }}" required>
+        <input id="email" type="email" class="h4 text-ribbon-input text-ribbon-primary{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email" required>
     </div>
 
-    <div class="form-group row">
-        <label for="email" class="col-lg-4 col-form-label text-lg-right">E-Mail Address</label>
+    <textarea name="message" id="message" class="h4 text-ribbon-input text-ribbon-input-half text-ribbon-primary" cols="30" rows="5" placeholder="Message">{{ old('message') }}</textarea>
 
-        <div class="col-lg-6">
-            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-            @if ($errors->has('email'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </div>
-            @endif
+    @if ($errors->has('name'))
+        <div class="invalid-feedback">
+            <strong>{{ $errors->first('name') }}</strong>
         </div>
-    </div>
-
-    <div class="form-group row">
-        <label for="message" class="col-lg-4 col-form-label text-lg-right">Message</label>
-
-        <div class="col-lg-6">
-            <textarea id="message" class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" name="message" required>
-                {{ old('message') }}
-            </textarea>
-
-            @if ($errors->has('message'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('message') }}</strong>
-                </div>
-            @endif
+    @endif
+    @if ($errors->has('email'))
+        <div class="invalid-feedback">
+            <strong>{{ $errors->first('email') }}</strong>
         </div>
-    </div>
-
-    <div class="form-group row">
-        <div class="col-lg-8 offset-lg-4">
-            <button type="submit" class="btn btn-primary">
-                Submit
-            </button>
+    @endif
+    @if ($errors->has('message'))
+        <div class="invalid-feedback">
+            <strong>{{ $errors->first('message') }}</strong>
         </div>
-    </div>
+    @endif
+
+    <button type="submit" class="btn btn-square btn-primary btn-lg btn-block">
+        Send Message
+    </button>
 </form>
---
+++ resources/views/public/partials/navbar.blade.php
@@ -0,0 +1,69 @@
+<div class="stage-shelf hidden" id="sidebar">
+    <ul class="nav nav-bordered nav-stacked flex-column">
+        <li class="nav-header">
+            <small>Events</small>
+        </li>
+
+        <li class="nav-item">
+            <a href="https://www.facebook.com/Diva-Classic-All-Womens-Event-401063200081726" target="_blank"
+               class="nav-link">Diva Classic</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://www.facebook.com/events/1705643622780477" target="_blank" class="nav-link">Michigan Roar
+                Natural Classic</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://www.facebook.com/events/1249702451737632" target="_blank" class="nav-link">Michigan
+                State</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://www.facebook.com/Minnesota-Mayhem-Natural-Bodybuilding-Championships-545085625591026"
+               target="_blank" class="nav-link">Minnesota Mayhem</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://www.facebook.com/events/298381637330687" target="_blank" class="nav-link">MTS Nutrition
+                Inferno</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://www.facebook.com/events/480484935664682" target="_blank" class="nav-link">Wisconsin Warrior
+                Natural Classic</a>
+        </li>
+
+        <li class="nav-divider"></li>
+
+        <li class="nav-header">
+            <small>Results</small>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">2018 Results</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">2017 Results</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">2016 Results</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">2015 Results</a>
+        </li>
+
+        <li class="nav-divider"></li>
+
+        <li class="nav-header">
+            <small>Extras</small>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#grid" data-target="#stage" data-toggle="stage">Grid</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#contact" data-target="#stage" data-toggle="stage">Contact Us</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#pricing" data-target="#stage" data-toggle="stage">Pricing</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#footer" data-target="#stage" data-toggle="stage">About</a>
+        </li>
+    </ul>
+</div>
