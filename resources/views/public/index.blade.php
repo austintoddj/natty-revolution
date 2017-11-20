1e13874029bd72a4272887e5a9bcb199beb9344e
Todd Austin <austin.todd.j@gmail.com>
2017-11-20 12:07:05 -0600

    Refactored view names

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -4,14 +4,14 @@
     @include('public.partials.sidebar')
     <div class="stage" id="stage">
         @include('public.partials.hero')
-        @include('public.partials.blocks')
+        @include('public.partials.featured')
         @include('public.partials.best-of-the-best')
         @include('public.partials.your-show')
         @include('public.partials.contact')
         @include('public.partials.coaching')
         @include('public.partials.footer')
-        @include('public.partials.modals.our-purpose')
-        @include('public.partials.modals.the-athletes')
-        @include('public.partials.modals.the-politics')
+        @include('public.partials.modals.purpose')
+        @include('public.partials.modals.athletes')
+        @include('public.partials.modals.politics')
     </div>
 @endsection
